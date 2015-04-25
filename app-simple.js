ab94c1cfbe25f6e7e8f33ed1c98c74b2241b5ae0
Agustin Palmieri <aguja13@hotmail.com>
2015-04-25 10:09:37 -0300

    primer commit

---
+++ app-simple.js
@@ -0,0 +1,13 @@
+var request = require('request'),
+    cheerio = require('cheerio');
+
+	request({url: 'http://thehackernews.com/', encoding: 'binary'}, function(err, resp, body){
+		if(!err && resp.statusCode == 200){
+			var $ = cheerio.load(body);
+			$('.blog-posts .hnews h2 a').each(function(){
+				var titulo = $(this).html();
+				console.log(titulo);
+			});
+
+		}
+	});
